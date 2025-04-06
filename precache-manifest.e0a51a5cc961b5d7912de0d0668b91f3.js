8b271d1ba88ebd1c02bf50572cc3c6802297be3d
Nobutan97 <your.email@example.com>
2025-04-06 22:12:38 +0900

    Updates

---
+++ precache-manifest.e0a51a5cc961b5d7912de0d0668b91f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754685484198c157fa14e40b355312a4",
+    "url": "/Skillpartner/index.html"
+  },
+  {
+    "revision": "0448c6c69ecfbfab03a2",
+    "url": "/Skillpartner/static/css/main.0951304b.chunk.css"
+  },
+  {
+    "revision": "c15dd92d11b5d97e30d0",
+    "url": "/Skillpartner/static/js/2.6ae5a7df.chunk.js"
+  },
+  {
+    "revision": "c4a92d83bff9d017a1c3",
+    "url": "/Skillpartner/static/js/3.d637a5b4.chunk.js"
+  },
+  {
+    "revision": "0448c6c69ecfbfab03a2",
+    "url": "/Skillpartner/static/js/main.75a2b248.chunk.js"
+  },
+  {
+    "revision": "84b67d315a7c0ccb336b",
+    "url": "/Skillpartner/static/js/runtime~main.734db104.js"
+  }
+]);
